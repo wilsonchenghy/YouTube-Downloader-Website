d76542f0b637e8c804b0976c7d00a512650b19ad
wilsonchenghy <150957380+wilsonchenghy@users.noreply.github.com>
2024-01-19 17:53:33 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, redirect, request
+from pytube import YouTube
+import subprocess
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def start():
+    return render_template("index.html") # through this app.py render the html
+
+
+@app.route('/download', methods=['POST'])
+def download():
+    try:
+        YouTubeUrl = request.form['youTubeUrl']
+        yt = YouTube(YouTubeUrl)
+        video = yt.streams.get_highest_resolution()
+
+        # find the user's download directory, if doesn't exist, then create it for them
+        userHomeDir = os.path.expanduser("~")
+        userDownloadDir = os.path.join(userHomeDir, "Downloads")
+        os.makedirs(userDownloadDir, exist_ok=True)
+        targetDownloadPath = userDownloadDir
+
+        videoPath = video.download(targetDownloadPath)
+        print(videoPath)
+        audioPath = videoPath.replace(".mp4", ".mp3")
+        print(audioPath)
+
+        subprocess.run(['ffmpeg', '-i', videoPath, '-q:a', '0', '-map', 'a', audioPath])
+
+        print("Success")
+        return redirect('/')
+    
+    except Exception as e:
+        return f"Error Occurred: {e}"
+
+
+# flask run will successfully run the server without the code below, but python3 app.py can only run the server successfully with the code below
+if __name__ == '__main__':
+    app.run(debug = False)
